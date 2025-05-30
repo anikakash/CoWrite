b610e234b27b65ae8edb16c992c35fa1c97efa69
anikakash <anik15-14210@diu.eud.bd>
2025-05-30 13:48:26 +0600

    add : get blog by user

--- src/Hooks/api.js
+++ src/Hooks/api.js
@@ -97,3 +97,26 @@ export const useGetBlog = (sortedValue) => {
 
   return {recentBlog, loading, error}
 };
+
+
+export const useGetBlogByAuthor =  (id) =>{
+ const [recentBlog, setRecentBlog] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const getRecentArticles = async () => {
+      setLoading(true);
+      try {
+        const res = await api.get(`/articals?userId=${id}`)
+        setRecentBlog(res.data);
+        console.log(res.data);
+        console.log("Author ID: ", id);
+      } catch (err) {
+        console.log("Error fetching recent articles: ", err);
+      } finally {
+        setLoading(false);
+      }
+    };
+    getRecentArticles();
+  }, [id]);
+  return {recentBlog, loading};
+}
