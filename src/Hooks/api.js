3873eaf05b8a88cc9c3358f60d99057dd8420e18
anikakash <anik15-14210@diu.eud.bd>
2025-05-30 13:55:32 +0600

    add : fetch comment api hook

--- src/Hooks/api.js
+++ src/Hooks/api.js
@@ -120,3 +120,28 @@ export const useGetBlogByAuthor =  (id) =>{
   }, [id]);
   return {recentBlog, loading};
 }
+
+export const useGetComment = ( articleId, userId)=>{
+const [comments, setComments] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const getComment = async () => {
+      try {
+        const res = await fetchData(`/comments`,{
+          userId: userId,
+articleId: articleId,
+
+        })
+        setComments(res.data);
+      } catch (err) {
+        console.log("Error fetching comments: ", err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    getComment();
+  }, [articleId, userId]);
+
+  return {comments, loading};
+}
